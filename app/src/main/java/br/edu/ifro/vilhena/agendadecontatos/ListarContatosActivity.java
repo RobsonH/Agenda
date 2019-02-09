88be2ac34b2a1805dd3b4e34959e20618c6d3739
RobsonH <robsonads531@gmail.com>
2019-02-08 21:55:12 -0400

    Aula de Android

---
+++ app/src/main/java/br/edu/ifro/vilhena/agendadecontatos/ListarContatosActivity.java
@@ -0,0 +1,48 @@
+package br.edu.ifro.vilhena.agendadecontatos;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListarContatosActivity extends AppCompatActivity {
+
+    private ListView listarContatosListView;
+    private FloatingActionButton listarContatosBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_contatos);
+
+        //Mapeando o componente da Activity
+        listarContatosListView = findViewById(R.id.listar_contatos_listview);
+        listarContatosBtn = findViewById(R.id.listar_contato_btn);
+
+        //Criando um adapter para enviar os dados para o ListView
+        String [] contatos = {"Robson", "João", "Maria", "José"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contatos);
+
+        //ListView utiliza o adapter
+        listarContatosListView.setAdapter(adapter);
+
+        // Ao clicar no floatActionButton
+        listarContatosBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ListarContatosActivity.this, FormularioActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
+
+
+    }
+}
